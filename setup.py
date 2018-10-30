6961b9ec39937dc13f2b9fb74d5fd57d5751135e
yuiant <yuiant@qq.com>
2018-10-31 00:25:15 +0800

    init

---
+++ setup.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+setup(
+    name='okex_v3',
+    description='okex_v3',
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    test_suite="tests",
+    zip_safe=False)
